4b06e9dd8555b280066b7d05b5305bda3a3b3353
J <68155263+jxxxxe@users.noreply.github.com>
2025-01-16 21:28:50 +0900

    Design: 로그인 로고 분리

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,16 +1,10 @@
+import LoadingIcon from '@/shared/components/loading-icon';
 import Image from 'next/image';
 
 const PageLoading = () => {
   return (
     <div className="wrap flex w-full flex-col items-center justify-center gap-3">
-      <Image
-        width={100}
-        height={100}
-        src={'/logo-removebg.png'}
-        alt="honterview-logo"
-        className="animate-bounce opacity-50"
-      />
-      <span className="font-mono text-2xl opacity-50">Loading...</span>
+      <LoadingIcon />
     </div>
   );
 };
