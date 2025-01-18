5c66575931d03f87695f792b241292662ba606b8
J <68155263+jxxxxe@users.noreply.github.com>
2025-01-19 00:23:01 +0900

    Design: 에러, 404페이지 디자인 수정

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,12 +1,44 @@
 'use client';
 
-import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
+import Button, { ButtonType } from '@/shared/components/button';
+import Logo from '@/shared/components/logo';
+import { ArrowPathIcon } from '@heroicons/react/24/outline';
+import { ExclamationTriangleIcon } from '@heroicons/react/24/solid';
+import { useRouter } from 'next/navigation';
 
 const ErrorPage = () => {
+  const router = useRouter();
+
+  const handlePrevPage = () => {
+    router.back();
+  };
+
+  const handleRefreshPage = () => {
+    router.refresh();
+  };
+
   return (
-    <div className="h-full w-full">
-      <ExclamationTriangleIcon />
-      에러가 발생했습니다.
+    <div className="fit-wrap flex flex-col items-center justify-center gap-5">
+      <div className="flex flex-col items-center justify-center">
+        <ExclamationTriangleIcon className="mb-[-1rem] size-16 text-red-500" />
+        <Logo
+          width={150}
+          height={150}
+        />
+      </div>
+      <h1 className="text-center text-[2.5rem] font-semibold">
+        에러가 발생했습니다
+      </h1>
+      <div className="flex items-center gap-5">
+        <Button
+          styleType={ButtonType.Type3}
+          onClick={handleRefreshPage}
+          className="flex h-auto w-auto gap-3 px-[1rem] py-[0.8rem] text-small text-white md:text-medium"
+        >
+          새로고침
+          <ArrowPathIcon className="size-8" />
+        </Button>
+      </div>
     </div>
   );
 };
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,9 @@
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
-import Button from '@/shared/components/button';
+import Button, { ButtonType } from '@/shared/components/button';
 import Logo from '@/shared/components/logo';
+import { XCircleIcon, XMarkIcon } from '@heroicons/react/24/solid';
 
 const NotFound = () => {
   const router = useRouter();
@@ -14,24 +15,34 @@ const NotFound = () => {
   };
 
   return (
-    <div className="fit-wrap flex flex-col items-center justify-center gap-16">
-      <Logo
-        width={150}
-        height={150}
-      />
-      <h1 className="text-center text-[3.6rem]">존재하지 않는 페이지입니다.</h1>
-      <div className="flex items-center gap-16">
-        <Link href="/">
-          <Button className="h-auto w-auto px-[1rem] py-[0.5rem] text-small text-white md:text-medium">
-            메인 페이지
-          </Button>
-        </Link>
+    <div className="fit-wrap flex flex-col items-center justify-center gap-5">
+      <div className="flex flex-col items-center justify-center">
+        <XCircleIcon className="mb-[-1rem] size-16 text-neutral-500 opacity-50" />
+        <Logo
+          className="opacity-50"
+          width={150}
+          height={150}
+        />
+      </div>
+      <h1 className="text-center text-[2.5rem] font-semibold">
+        존재하지 않는 페이지입니다
+      </h1>
+      <div className="flex items-center gap-5">
         <Button
+          styleType={ButtonType.Type3}
           onClick={handlePrevPage}
-          className="h-auto w-auto px-[1rem] py-[0.5rem] text-small text-white md:text-medium"
+          className="h-auto w-auto px-[1rem] py-[0.8rem] text-small text-white md:text-medium"
         >
           이전 페이지
         </Button>
+        <Link href="/">
+          <Button
+            styleType={ButtonType.Type3}
+            className="h-auto w-auto px-[1rem] py-[0.8rem] text-small text-white md:text-medium"
+          >
+            메인 페이지
+          </Button>
+        </Link>
       </div>
     </div>
   );
