355a67e5bac467e737b0c5e6e3a479ba1479ea99
J <68155263+jxxxxe@users.noreply.github.com>
2025-01-25 23:54:41 +0900

    File: 인터뷰 api 파일 구조 변경

--- src/app/api/interview/openai/question/route.ts
+++ src/app/api/interview/openai/question/route.ts
@@ -1,34 +1,28 @@
+import openai from '@/shared/libs/openai';
 import { NextRequest, NextResponse } from 'next/server';
-import OpenAI from 'openai';
 import { ChatCompletionMessageParam } from 'openai/resources';
-
-// OpenAI 클라이언트 초기화
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-  baseURL: 'https://api.perplexity.ai',
-});
 
 export async function POST(req: NextRequest) {
   try {
-    const { questions, myResponses } = await req.json(); // 클라 요청을 JSON 객체로 파싱 후 메세지 얻기
+    const { prevQuestion, prevResponse, firstQuestionTagList } =
+      await req.json(); // 클라 요청을 JSON 객체로 파싱 후 메세지 얻기
     const command = `저는 개발자 면접중이고, 당신은 제 면접관입니다.`;
 
     const messages: ChatCompletionMessageParam[] = [
-      { role: 'system', content: command },
-      { role: 'user', content: '면접 시작하겠습니다.' },
+      {
+        role: 'user',
+        content: `저는 개발자 면접중이고, 당신은 제 면접관입니다. ${firstQuestionTagList}와 관련해 질문해주세요.`,
+      },
     ];
 
-    questions.forEach((question, idx) => {
-      messages.push(
-        { role: 'assistant', content: question },
-        {
-          role: 'user',
-          content: `${myResponses[idx]} 제 답에 대해 꼬리 질문을 한줄로 간략하게 해주세요.`,
-        },
-      );
-    });
+    messages.push(
+      { role: 'assistant', content: prevQuestion },
+      {
+        role: 'user',
+        content: `${prevResponse} 제 답에 대해 꼬리 질문을 한줄로 간략하게 해주세요.`,
+      },
+    );
 
-    console.log(messages);
     const response = await openai.chat.completions.create({
       model: 'sonar',
       messages,
